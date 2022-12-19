3d32bf4910889427953fc8ed3fe62ab4018d1536
Felipe Ribeiro <felipe.ribeiro@rdamasio.com.br>
2022-12-19 09:27:50 -0300

    19/12/2022

--- components/modalCreateUpdateColor.vue
+++ components/modalCreateUpdateColor.vue
@@ -1,12 +1,16 @@
 <template>
-  
+  <photoshop-picker v-model="colors"/>
 </template>
 
 <script lang="ts">
+//@ts-ignore
+import { Photoshop } from 'vue-color'
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
+  components: {
+    'photoshop-picker': Photoshop
+  },
   data() {
     return {
       colors: '#000'
